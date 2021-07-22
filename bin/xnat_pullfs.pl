7770eff343e062498d60cba16f010131cc0b58e8
O. Sotolongo <44894699+asqwerty666@users.noreply.github.com>
2021-07-22 10:02:48 +0200

    add license

--- bin/xnat_pullfs.pl
+++ bin/xnat_pullfs.pl
@@ -1,6 +1,16 @@
 #!/usr/bin/perl
 #Copyright 2021 O. Sotolongo <asqwerty@gmail.com>
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details
+
 # Este script captura los resultados de Freesurfer obtenidos en XNAT 
 # y los copia localmente en una nativa ed Freesurfer.
 # Esto es util para realizar procedimientos posteriores 
