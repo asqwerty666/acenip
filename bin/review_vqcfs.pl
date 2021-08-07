e87b9f3c00949c2b26ef119bcecd04af55988148
asqwerty666 <asqwerty@gmail.com>
2021-08-07 15:37:40 +0200

    fsqc added

--- bin/review_vqcfs.pl
+++ bin/review_vqcfs.pl
@@ -28,7 +28,7 @@ use NEURO4 qw(load_project print_help populate check_or_make getLoggingTime);
 
 my $prj;
 my $xprj;
-my $ifile;
+my $ifile; my $efile;
 my $wdir = cwd;
 my $odir;
 
@@ -37,43 +37,66 @@ while (@ARGV and $ARGV[0] =~ /^-/) {
 	$_ = shift;
 	last if /^--$/;
 	if (/^-i/) { $ifile = shift; chomp($ifile);}
-	if (/^-o/) { $odir = shift; chomp($odir);}
+	if (/^-d/) { $odir = shift; chomp($odir);}
 	if (/^-h/) { print_help $ENV{'PIPEDIR'}.'/doc/reviewqc.hlp'; exit;}
 	if (/^-x/) { $xprj = shift; chomp($xprj);} #nombre del proyecto en XNAT
 	if (/^-p/) { $prj = shift; chomp($prj);} #nombre local del proyecto
 }
 
-# Mira, hay que meter el proyecto de XNAt con alguno de los dos switch
+# Mira, hay que meter el proyecto de XNAT con alguno de los dos switch
 $xprj = $prj unless $xprj;
 # O te vas a tomar por culo
 die unless $xprj;
-# y entonces, el file de input es el que tu digas o el que toca que es.
-# Estos son los experimentos a los que vamos a revisar o hacerel QC. 
+# You must be under X11 to run this shit
+die "Should run this under X!\n" unless $ENV{'DISPLAY'};
+
+# y entonces, el file de input es el que tu digas o los tendras que mirar todos
+# Estos son los experimentos a los que vamos a revisar o hacer el QC. 
 # Puedes tomar la lista completa y reducirla a lo que quieres o simplemente 
 # tomar la lista as is
-$ifile=$wdir.'/'.$xprj.'_experiment.list' unless $ifile;
-unless ($odir) {
+my @pollos;
+$efile=$wdir.'/'.$xprj.'_experiment.list';
+if ($ifile and -f $ifile){
+	$efile=$wdir.'/'.$xprj.'_experiment_custom.list';
+	open IDF, "<$ifile";
+	@pollos = <IDF>; chomp @pollos;
+	close IDF;
+	open TDF, ">$efile";
+	foreach my $pollo (@pollos){
+		my $look = "xnatapic list_experiments --project_id $xprj --subject_id $pollo --modality MRI";
+		my $epollo = qx/$look/;
+		chomp $epollo;
+		print TDF "$epollo\n";
+	}
+	close TDF;
+}
+if ($odir and -d $odir) {
+	my $bckdir = $odir.'_'.getLoggingTime();
+	my $mvorder = "mv $odir $bckdir";
+	system($mvorder);
+}else{
 	#Si no me das el dir que tengo que leer entonces lo creo
 	$odir=$wdir.'/'.$xprj.'_fsresults';
 	if ( -d $odir ){
-		# a no ser que exita, entonces muevo este a otro sitio
+		# a no ser que exista, entonces muevo este a otro sitio
 		# y creo uno nuevo con el nombre por default
 		my $bckdir = $odir.'_'.getLoggingTime();
 		my $mvorder = "mv $odir $bckdir";
 		system($mvorder);
 	}
-	mkdir $odir; my $pre;
-	#aqui, si me das el archivo con los experimentos que tengo 
-	$pre = "xnatapic prepare_fsqc --project_id $xprj --list $ifile";
-	system($pre);
-}else{
-	unless ( -d $odir ){
-		mkdir $odir; my $pre;
-		$pre = "xnatapic prepare_fsqc --project_id $xprj --list $ifile";
-		system($pre);
-
-	}
+	mkdir $odir;
 }
+#y ejecuto la preparacion
+if ($ifile and -f $ifile){
+	foreach my $pollo (@pollos){
+		my $pre = "xnatapic prepare_fsqc --project_id $xprj --outdir $odir --subject_id $pollo";
+		system($pre);
+	}
+}else{	
+	my $pre = "xnatapic prepare_fsqc --project_id $xprj --outdir $odir";
+	system($pre);
+}
+
 my $vqcd = $wdir.'/visualqc_output';
 if ( -d $vqcd ){
 	my $bckdir = $vqcd.'_'.getLoggingTime();
@@ -81,8 +104,8 @@ if ( -d $vqcd ){
 	system($mvorder);
 }
 mkdir $vqcd;
-my $order = "vqcfs -i $ifile -f $odir -o $vqcd";
+my $order = "vqcfs -i $efile -f $odir -o $vqcd";
 system($order);
 
 my $post = "xnatapic upload_fsqc --qcdir $vqcd";
-system($order);
+system($post);
