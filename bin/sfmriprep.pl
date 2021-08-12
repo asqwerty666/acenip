37349d47fe0bc73f4deee47c44cf2a9291d2238f
asqwerty666 <asqwerty@gmail.com>
2021-08-12 16:10:54 +0200

    SLURM library calls added

--- bin/sfmriprep.pl
+++ bin/sfmriprep.pl
@@ -12,6 +12,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
+#
 use strict; use warnings;
  
 use File::Find::Rule;
@@ -20,9 +21,10 @@ use Data::Dump qw(dump);
 use File::Remove 'remove';
 use File::Basename qw(basename);
 use File::Copy::Recursive qw(dircopy);
-
+use SLURM qw(send2slurm);
 my $fslic = '/nas/usr/local/opt/freesurfer/.license';
 my $fmriprep_version = '1.5.8';
+my $fmriprep_simg = '/usr/local/bin/fmriprep-latest.simg';
 my $cfile="";
 my $fs = 0;
 my $st = 0;
@@ -61,7 +63,13 @@ my $outdir = "$std{'DATA'}/slurm";
 check_or_make($outdir);
 
 my @subjects = cut_shit($db, $data_dir."/".$cfile);
-
+my %ptask = ('job_name' => 'fmriprep_'.$study,
+	'time' => '72:0:0',
+	'mailtype' => 'FAIL,TIME_LIMIT,STAGE_OUT',
+	'cpus' => 16,
+	'mem_per_cpu' => '4G',
+	'partition' => 'fast',
+);
 foreach my $subject (@subjects) {
 	my %nifti = check_subj($std{'DATA'},$subject);
 	if($nifti{'func'}){
@@ -71,33 +79,16 @@ foreach my $subject (@subjects) {
 			check_or_make($fssd);
 			dircopy($rfssd,$fssd);
 		#}
-		my $orderfile = $outdir.'/'.$subject.'_fmriprep.sh';
-		open ORD, ">$orderfile";
-		print ORD '#!/bin/bash'."\n";
-		print ORD '#SBATCH -J fmriprep_'.$study."\n";
-		print ORD '#SBATCH --time=72:0:0'."\n"; #si no ha terminado en X horas matalo
-		print ORD '#SBATCH --mail-type=FAIL,TIME_LIMIT,STAGE_OUT'."\n"; #no quieres que te mande email de todo
-		print ORD '#SBATCH -o '.$outdir.'/fmriprep-%j'."\n";
-		print ORD '#SBATCH -c 16'."\n";
-		print ORD '#SBATCH --mem-per-cpu=4G'."\n";
-		print ORD '#SBATCH -p fast'."\n";
-		print ORD '#SBATCH --mail-user='."$ENV{'USER'}\n";
-		print ORD 'singularity run --cleanenv -B /nas:/nas /usr/local/bin/fmriprep-latest.simg '.$bids_dir.' '.$fmriout_dir.' participant --participant-label '.$subject.' '.$noslicetiming.' --skip_bids_validation --fs-license-file '.$fslic.' --nthreads 16 --omp-nthreads 8 --mem-mb 30000 --output-spaces T1w MNI152Lin fsnative --use-aroma -w '.$fmriwork_dir;
-		print ORD "\n";
-		close ORD;
-		system("sbatch $orderfile");
-		#sleep(20);
+		$ptask{'filename'} = $outdir.'/'.$subject.'_fmriprep.sh';
+		$ptask{'job_name'} = 'fmriprep_'.$study;
+		$ptask{'output'} = $outdir.'/fmriprep-%j';
+		$ptask{'command'} = 'singularity run --cleanenv -B /nas:/nas '.$fmriprep_simg.' '.$bids_dir.' '.$fmriout_dir.' participant --participant-label '.$subject.' '.$noslicetiming.' --skip_bids_validation --fs-license-file '.$fslic.' --nthreads 16 --omp-nthreads 8 --mem-mb 30000 --output-spaces T1w MNI152Lin fsnative --use-aroma -w '.$fmriwork_dir;
+		send2slurm(\%ptask);
 	}
 }
-my $orderfile = $outdir.'/fmriprep_end.sh';
-open ORD, ">$orderfile";
-print ORD '#!/bin/bash'."\n";
-print ORD '#SBATCH -J fmriprep_'.$study."\n";
-print ORD '#SBATCH --mail-type=END'."\n"; #email cuando termine
-print ORD '#SBATCH --mail-user='."$ENV{'USER'}\n";
-print ORD '#SBATCH -p fast'."\n";
-print ORD '#SBATCH -o '.$outdir.'/fmriprep_end-%j'."\n";
-print ORD ":\n";
-close ORD;
-my $order = 'sbatch --dependency=singleton '.$orderfile;
-exec($order);
+my %final = ( 'filename' => $outdir.'/fmriprep_end.sh',
+	'job_name' => 'fmriprep_'.$study,
+	'output' => $outdir.'/fmriprep_end-%j',
+	'dependency' => 'singleton',
+);
+send2slurm(\%final);
