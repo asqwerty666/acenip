c0117b3948766bcec6e79e949861918b579a30df
asqwerty666 <asqwerty@gmail.com>
2021-08-12 15:33:08 +0200

    SLURM library calls added

--- bin/plong.pl
+++ bin/plong.pl
@@ -12,10 +12,32 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
+# Aqui la intencion es hacer un analisis longitudinal con Freesurfer
+# Ver https://detritus.fundacioace.com/wiki/doku.php?id=neuroimagen:fs_long_analysis
+#
+# Se supone que para proceder he de tener un archivo relacionando los 
+# distintos puntos del analisis. En principio lo he probado con dos puntos 
+# pero el algoritmo esta escrito para N puntos. Solo habra que tener un
+# archivo con un formato similar a este,
+#
+# flong_subject;SubjID_v0;SubjID_v2
+# flong_0001;facehbi_0001;f2cehbi_0001
+# flong_0002;facehbi_0002;f2cehbi_0002
+# flong_0003;facehbi_0003;f2cehbi_0003
+#
+# Entonces deberia crear el proyecto longitudinal de la forma usual,
+#
+# make_proj lfacehbi /nas/corachan/facehbi
+#
+# y ejecutar este script 
+#
+# plong.pl -i flong.csv lfacehbi
+#
+
 use strict; use warnings;
 use File::Basename qw(basename);
 use Data::Dump qw(dump);
-
+use SLURM qw(send2slurm);
 use NEURO4 qw(populate check_subj load_project print_help check_or_make get_subjects get_list);
 
 my $ifile;
@@ -29,10 +51,11 @@ while (@ARGV and $ARGV[0] =~ /^-/) {
 }
 
 my $study = shift;
+# Debo tener el archivo longitudinal y el nombre del proyecto definidos
 unless ($ifile && $study) { print_help $ENV{'PIPEDIR'}.'/doc/longitudinal.hlp'; exit;}
 my $debug = 1;
 my %lsubjects;
-
+# Aqui lo que hago es meter en un HoA los puntos longitudinales
 open ILF, "<$ifile" or die "Could not open input data file\n";
 while(<ILF>){
 	my @long_list = split /;/, $_;
@@ -41,6 +64,7 @@ while(<ILF>){
 	$lsubjects{$lindex} = [ @long_list ];
 }
 close ILF;
+# y cargo los datos del proyecto
 my %std = load_project($study);
 my $data_dir=$std{'DATA'};
 #open debug file
@@ -50,58 +74,39 @@ $debug ? open DBG, ">$logfile" :0;
 my $outdir = "$std{'DATA'}/slurm";
 check_or_make($outdir);
 
+my %ptask = ('job_name' => 'fs_long_'.$study,
+	'time' => '48:0:0',
+	'mailtype' => 'FAIL,TIME_LIMIT,STAGE_OUT',
+	'partition' => 'fast',
+	'cpus' => 4,
+	'mem_per_cpu' => '4G',	
+);
 foreach my $lsubject (sort keys %lsubjects){
 	my $sub_check_dir = $ENV{'SUBJECTS_DIR'}.'/'.$lsubjects{$lsubject}[0];
 	if( -e $sub_check_dir && -d $sub_check_dir){
 		my $subol = join ' -tp ', @{$lsubjects{$lsubject}};
 		chomp $subol;
-		my $order = "recon-all -base ".$lsubject." -tp ".$subol." -all";
-		my $orderfile = $outdir.'/'.$lsubject.'_fs_long_base.sh';
-		open ORD, ">$orderfile";
-		print ORD '#!/bin/bash'."\n";
-		print ORD '#SBATCH -J fs_long_'.$study."\n";
-		print ORD '#SBATCH --time=48:0:0'."\n"; #si no ha terminado en X horas matalo
-		print ORD '#SBATCH --mail-type=FAIL,TIME_LIMIT,STAGE_OUT'."\n"; #no quieres que te mande email de todo
-		print ORD '#SBATCH --mail-user='."$ENV{'USER'}\n";
-		print ORD '#SBATCH -p fast'."\n";
-		print ORD '#SBATCH -c 4'."\n";
-		print ORD '#SBATCH --mem-per-cpu=4G'."\n";
-		print ORD '#SBATCH -o '.$outdir.'/fs_long_base-slurm-'.$lsubject.'-%j'."\n";
-		print ORD "srun $order\n";
-		close ORD;
-		my $jobid = `sbatch $orderfile`;
-		$jobid = ( split ' ', $jobid )[ -1 ];
+		$ptask{'command'} = "recon-all -base ".$lsubject." -tp ".$subol." -all";
+		$ptask{'filename'} = $outdir.'/'.$lsubject.'_fs_long_base.sh';
+		$ptask{'output'} = $outdir.'/fs_long_base-slurm-'.$lsubject.'-%j';
+		$ptask{'dependency'} = '';
+		my $jobid = send2slurm(\%ptask);
 		foreach my $ind_subject (sort @{$lsubjects{$lsubject}}){
 			chomp $ind_subject;
-			my $order = "recon-all -long ".$ind_subject." ".$lsubject." -all";
-			my $orderfile = $outdir.'/'.$lsubject.'_'.$ind_subject.'_fs_long_base.sh';
-			open IORD, ">$orderfile";
-			print IORD '#!/bin/bash'."\n";
-			print IORD '#SBATCH -J fs_long_'.$study."\n";
-			print IORD '#SBATCH --time=48:0:0'."\n"; #si no ha terminado en X horas matalo
-			print IORD '#SBATCH --mail-type=FAIL,TIME_LIMIT,STAGE_OUT'."\n"; #no quieres que te mande email de todo
-			print IORD '#SBATCH --mail-user='."$ENV{'USER'}\n";
-			print IORD '#SBATCH -p fast'."\n";
-			print IORD '#SBATCH -c 4'."\n";
-			print IORD '#SBATCH --mem-per-cpu=4G'."\n";
-			print IORD '#SBATCH -o '.$outdir.'/fs_long_ind-slurm-'.$lsubject.'_'.$ind_subject.'-%j'."\n";
-			print IORD "srun $order\n";
-			close IORD;
-			system("sbatch --dependency=afterok:$jobid $orderfile");
+			$ptask{'command'} = "recon-all -long ".$ind_subject." ".$lsubject." -all";
+			$ptask{'filename'} = $outdir.'/'.$lsubject.'_'.$ind_subject.'_fs_long_base.sh';
+			$ptask{'output'} = $outdir.'/fs_long_ind-slurm-'.$lsubject.'_'.$ind_subject.'-%j';
+			$ptask{'dependency'} = "afterok:$jobid";
+			send2slurm(\%ptask);
 		}
 		sleep(2);
 	}
 }
 
 $debug ? close DBG:0;	
-my $orderfile = $outdir.'/fs_long_end.sh';
-open ORD, ">$orderfile";
-print ORD '#!/bin/bash'."\n";
-print ORD '#SBATCH -J fs_long_'.$study."\n";
-print ORD '#SBATCH --mail-type=END'."\n"; #email cuando termine
-print ORD '#SBATCH --mail-user='."$ENV{'USER'}\n";
-print ORD '#SBATCH -o '.$outdir.'/fs_long_end-%j'."\n";
-print ORD ":\n";
-close ORD;
-my $order = 'sbatch --dependency=singleton '.$orderfile;
-exec($order);
+my %final = ('filename' => $outdir.'/fs_long_end.sh',
+	'job_name' => 'fs_long_'.$study,
+	'output' => $outdir.'/fs_long_end-%j',
+	'dependency' => 'singleton',
+);
+send2slurm(\%final);
--- bin/pqcache.pl
+++ bin/pqcache.pl
@@ -12,10 +12,23 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
+# El objetivo de este script es preparar el proyecto para hacer un SBM.
+# O dicho de otra manera, el Freesurfer Group Analysis (FSGA) se hace mas comodamente
+# si se antes se ejecuta el recon-all con la opcion -qcache 
+#
+# Segun https://surfer.nmr.mgh.harvard.edu/fswiki/FsTutorial/GroupAnalysis:
+# When you run recon-all with the -qcache option, recon-all will resample data 
+# onto the average subject (called fsaverage) and smooth it at various FWHM 
+# (full-width/half-max) values, usually 0, 5, 10, 15, 20, and 25mm. This can 
+# speed later processing. 
+# 
+# Asi que aqui paralelizamos el recon-all -qcache y vamos a suponer que ya hemos
+# ejecutado la segmentacion, o la hemos bajado de XNAT con xnat_getfs.pl o similar. 
+
 use strict; use warnings;
 use File::Basename qw(basename);
 use Data::Dump qw(dump);
-
+use SLURM qw(send2slurm);
 use NEURO4 qw(populate check_subj load_project print_help check_or_make get_subjects get_list);
 
 my $cfile;
@@ -27,7 +40,7 @@ while (@ARGV and $ARGV[0] =~ /^-/) {
     if (/^-cut/) { $cfile = shift; chomp($cfile);}
     if (/^-h/) { print_help $ENV{'PIPEDIR'}.'/doc/recon.hlp'; exit;}
 }
-
+# Hacemos lo usual, leemos el proyecto, configuramos el workspace, etc
 my $study = shift;
 unless ($study) { print_help $ENV{'PIPEDIR'}.'/doc/recon.hlp'; exit;}
 my $debug = 1;
@@ -60,38 +73,29 @@ if ($cfile){
 }else{
 	@plist = @iplist;
 }
+my %ptask = ( 'job_name' => 'fs_qcache_'.$study,
+	'time' => '48:0:0',
+	'cpus' => 4,
+	'mem_per_cpu' => '4G',
+	'mailtype' => 'FAIL,TIME_LIMIT,STAGE_OUT',
+	'partition' => 'fast',
+);
 foreach my $pkey (sort @plist){
 	my $subj = $study."_".$pkey;
 	my $subj_dir = $ENV{'SUBJECTS_DIR'}.'/'.$subj;
 	if( -e $subj_dir && -d $subj_dir){
-		my $order = "recon-all -subjid ".$subj." -qcache";
-		my $orderfile = $outdir.'/'.$subj.'fs_qcache.sh';
-		open ORD, ">$orderfile";
-		print ORD '#!/bin/bash'."\n";
-		print ORD '#SBATCH -J fs_qcache_'.$study."\n";
-		print ORD '#SBATCH --time=48:0:0'."\n"; #si no ha terminado en X horas matalo
-		print ORD '#SBATCH --mail-type=FAIL,TIME_LIMIT,STAGE_OUT'."\n"; #no quieres que te mande email de todo
-		print ORD '#SBATCH --mail-user='."$ENV{'USER'}\n";
-		print ORD '#SBATCH -p fast'."\n";
-		print ORD '#SBATCH -c 4'."\n";
-		print ORD '#SBATCH --mem-per-cpu=4G'."\n";
-		print ORD '#SBATCH -o '.$outdir.'/fs_qcache-slurm-'.$subj.'-%j'."\n";
-		print ORD "srun $order\n";
-		close ORD;
-		system("sbatch $orderfile");
+		$ptask{'command'} = "recon-all -subjid ".$subj." -qcache";
+		$ptask{'filename'}  = $outdir.'/'.$subj.'fs_qcache.sh';
+		$ptask{'output'} = $outdir.'/fs_qcache-slurm-'.$subj.'-%j';
+		send2slurm(\%ptask);
 		sleep(2);
 	}
 }
 
 $debug ? close DBG:0;	
-my $orderfile = $outdir.'/fs_qcache_end.sh';
-open ORD, ">$orderfile";
-print ORD '#!/bin/bash'."\n";
-print ORD '#SBATCH -J fs_qcache_'.$study."\n";
-print ORD '#SBATCH --mail-type=END'."\n"; #email cuando termine
-print ORD '#SBATCH --mail-user='."$ENV{'USER'}\n";
-print ORD '#SBATCH -o '.$outdir.'/fs_recon_end-%j'."\n";
-print ORD ":\n";
-close ORD;
-my $order = 'sbatch --dependency=singleton '.$orderfile;
-exec($order);
+my %final = ( 'filename' => $outdir.'/fs_qcache_end.sh',
+	'job_name' => 'fs_qcache_'.$study,
+	'output' => $outdir.'/fs_recon_end-%j',
+	'dependency' => 'singleton',
+);
+send2slurm(\%final);
