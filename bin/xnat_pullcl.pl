49e46b6ec98b9a96f9d6956bb368afe9cc69847f
asqwerty666 <asqwerty@gmail.com>
2022-02-15 23:06:32 +0100

    pull also dates

--- bin/xnat_pullcl.pl
+++ bin/xnat_pullcl.pl
@@ -12,6 +12,7 @@ my $STDOLD;
 my $ofile = '';
 chomp($tmpdir = `mktemp -d /tmp/petcl.XXXXXX`);
 my $guide = $tmpdir."/xnat_guys.list";
+my $with_date = 0;
 @ARGV = ("-h") unless @ARGV;
 while (@ARGV and $ARGV[0] =~ /^-/) {
     $_ = shift;
@@ -19,6 +20,7 @@ while (@ARGV and $ARGV[0] =~ /^-/) {
     if (/^-p/) { $prj = shift; chomp($prj);}
     if (/^-x/) { $xprj = shift; chomp($xprj);}
     if (/^-o/) { $ofile = shift; chomp($ofile);}
+    if (/^-d/) { $with_date = 1; }
     if (/^-h/) { print_help $ENV{'PIPEDIR'}.'/doc/xnat_pullcl.hlp'; exit;}
 }
 $xprj = $prj unless $xprj;
@@ -34,10 +36,30 @@ system($order);
 $order = "xnatapic get_registration_report --project_id ".$xprj." --output ".$tmpdir."/xnat_pet_results.csv".' 1>/dev/null';
 system($order);
 #print "Sorting, joining, keeping shit together\n";
-$order = "awk -F\",\" '{print \$2\",\"\$6\",\"\$7}' ".$tmpdir."/xnat_pet_results.csv | sed 's/\"//g' | tail -n +2 | sort -t, -k 1 > ".$tmpdir."/pet.results";
-system($order);
+#
+if($with_date){
+	$order = "awk -F\",\" '{print \$2\",\"\$3\",\"\$6\",\"\$7}' ".$tmpdir."/xnat_pet_results.csv | sed 's/\"//g' | tail -n +2 | sort -t, -k 1";
+	open IDF, "<$order" or die "Could not execute th order\n";
+	open ODF, ">$tmpdir.'/pet.results'";
+	while(<IDF>){
+		my ($sbj, $pxp, $suvr, $cl) = /(.*),(.*),(.*),(.*)/;
+		#xnatapic list_experiments --project_id f5cehbi --subject_id XNAT_S00086 --modality PET --date | awk -F',' '{print $2}'
+		my $sorder = 'xnatapic list_experiments --project_id '.$xprj.' --subject_id '.$sbj."--modality PET --date | awk -F',' '{print \$2}'";
+		my ($xdate) = qx/$sorder/;
+		print ODF "$sbj,$xdate,$suvr,$cl\n";
+	}
+	close ODF;
+	close IDF;
+}else{
+	$order = "awk -F\",\" '{print \$2\",\"\$6\",\"\$7}' ".$tmpdir."/xnat_pet_results.csv | sed 's/\"//g' | tail -n +2 | sort -t, -k 1 > ".$tmpdir."/pet.results";
+	system($order);
+}
 open STDOUT, ">$ofile" unless not $ofile;
-$order = "join -t, ".$tmpdir."/xnat_subjects_sorted.list ".$tmpdir."/pet.results | sort -t, -k 2 | awk -F\",\" '{if (\$3) print \$2\";\"\$3\";\"\$4}' | sed '1iSubject;SUVR;Centiloid'";
+if($with_date){
+	$order = "join -t, ".$tmpdir."/xnat_subjects_sorted.list ".$tmpdir."/pet.results | sort -t, -k 2 | awk -F\",\" '{if (\$3) print \$2\";\"\$3\";\"\$4\";\"\$5}' | sed '1iSubject;Date;SUVR;Centiloid'";
+}else{
+        $order = "join -t, ".$tmpdir."/xnat_subjects_sorted.list ".$tmpdir."/pet.results | sort -t, -k 2 | awk -F\",\" '{if (\$3) print \$2\";\"\$3\";\"\$4}' | sed '1iSubject;Date;SUVR;Centiloid'";
+}
 system($order);
 #$order = "sed 's/;/,/g' ".$proj_file." | sort -t, -k 2 > ".$tmpdir."/all_pet.list;"; 
 #$order.= "join -t, -j 2 ".$tmpdir."/all_pet.list ".$tmpdir."/xnat_tmp_pet_sorted.list | awk -F\",\" '{print \$2\";\"\$4\";\"\$5}' | sed '1iSubject;SUVR;Centiloid' > ".$std{'DATA'}."/xnat_fbb_cl.csv";
