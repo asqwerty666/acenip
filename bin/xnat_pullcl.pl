de1ea86ac5412f8118f4919a7913c1fbf03f1862
asqwerty666 <asqwerty@gmail.com>
2022-02-16 15:54:56 +0100

    pull also dates

--- bin/xnat_pullcl.pl
+++ bin/xnat_pullcl.pl
@@ -4,14 +4,15 @@
 use strict;
 use warnings;
 use NEURO4 qw(load_project print_help populate check_or_make);
-
+use File::Temp qw(tempdir);
 my $prj;
 my $xprj;
 my $tmpdir; 
 my $STDOLD;
 my $ofile = '';
-chomp($tmpdir = `mktemp -d /tmp/petcl.XXXXXX`);
-my $guide = $tmpdir."/xnat_guys.list";
+#my $tmp_dir = $ENV{'TMPDIR'};
+#my $tmpdir = tempdir(TEMPLATE => $tmp_dir.'/petcl.XXXXX', CLEANUP => 1);
+#my $guide = $tmpdir."/xnat_guys.list";
 my $with_date = 0;
 @ARGV = ("-h") unless @ARGV;
 while (@ARGV and $ARGV[0] =~ /^-/) {
@@ -28,6 +29,9 @@ die "Should supply XNAT project name\n" unless $xprj;
 #my %std = load_project($prj);
 #my $proj_file = $std{'DATA'}.'/'.$prj.'_mri.csv';
 #print "Wait a minute, getting XNAT subject list\n";
+my $tmp_dir = $ENV{'TMPDIR'};
+$tmpdir = tempdir(TEMPLATE => $tmp_dir.'/petcl.XXXXX', CLEANUP => 0);
+print "Working at $tmpdir\n";
 my $order = "xnatapic list_subjects --project_id ".$xprj." --label > ".$tmpdir."/xnat_subjects.list";
 system($order);
 $order = "sort -t, -k 1 ".$tmpdir."/xnat_subjects.list > ".$tmpdir."/xnat_subjects_sorted.list";
@@ -38,14 +42,18 @@ system($order);
 #print "Sorting, joining, keeping shit together\n";
 #
 if($with_date){
-	$order = "awk -F\",\" '{print \$2\",\"\$3\",\"\$6\",\"\$7}' ".$tmpdir."/xnat_pet_results.csv | sed 's/\"//g' | tail -n +2 | sort -t, -k 1";
-	open IDF, "<$order" or die "Could not execute th order\n";
-	open ODF, ">$tmpdir.'/pet.results'";
+	my $ptfile = $tmpdir.'/pet.results.tmp';
+	$order = "awk -F\",\" '{print \$2\",\"\$3\",\"\$6\",\"\$7}' ".$tmpdir."/xnat_pet_results.csv | sed 's/\"//g' | tail -n +2 | sort -t, -k 1 > ".$ptfile;
+	system($order);
+	open IDF, "<$ptfile" or die "No temporary file at all\n";
+	my $optfile = $tmpdir.'/pet.results';
+	open ODF, ">$optfile" or die "Could not open output file\n";
 	while(<IDF>){
 		my ($sbj, $pxp, $suvr, $cl) = /(.*),(.*),(.*),(.*)/;
 		#xnatapic list_experiments --project_id f5cehbi --subject_id XNAT_S00086 --modality PET --date | awk -F',' '{print $2}'
-		my $sorder = 'xnatapic list_experiments --project_id '.$xprj.' --subject_id '.$sbj."--modality PET --date | awk -F',' '{print \$2}'";
+		my $sorder = 'xnatapic list_experiments --project_id '.$xprj.' --subject_id '.$sbj." --experiment_id ".$pxp." --modality PET --date | awk -F',' '{print \$2}'";
 		my ($xdate) = qx/$sorder/;
+		chomp $xdate;
 		print ODF "$sbj,$xdate,$suvr,$cl\n";
 	}
 	close ODF;
