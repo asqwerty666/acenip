2ea5147374402cc4d6461ccc687a4d21cd91acba
asqwerty666 <asqwerty@gmail.com>
2022-05-27 09:27:37 +0200

    sorting shit, get experiment resources and so on

--- lib/XNATACE.pm
+++ lib/XNATACE.pm
@@ -18,8 +18,8 @@ require Exporter;
 use JSON qw(decode_json); 
 use Data::Dump qw(dump);
 our @ISA = qw(Exporter);
-our @EXPORT = qw(xconf xget_conf xget_pet xget_session xget_mri xput_rvr xput_report xget_rvr xget_rvr_data xget_subjects xget_pet_reg xget_fs_data xget_pet_data xget_exp_data xget_sbj_data xget_fs_stats xget_fs_qc);
-our @EXPORT_OK = qw(xconf xget_conf xget_pet xget_session xget_mri xput_rvr xput_report xget_rvr xget_rvr_data xget_subjects xget_pet_reg xget_fs_data xget_pet_data xget_exp_data xget_sbj_data xget_fs_stats xget_fs_qc);
+our @EXPORT = qw(xconf xget_conf xget_pet xget_session xget_mri xput_rvr xput_report xget_rvr xget_rvr_data xget_subjects xget_pet_reg xget_fs_data xget_pet_data xget_exp_data xget_sbj_data xget_fs_stats xget_fs_qc xget_fs_allstats);
+our @EXPORT_OK = qw(xconf xget_conf xget_pet xget_session xget_mri xput_rvr xput_report xget_rvr xget_rvr_data xget_subjects xget_pet_reg xget_fs_data xget_pet_data xget_exp_data xget_sbj_data xget_fs_stats xget_fs_qc xget_fs_allstats);
 our %EXPORT_TAGS =(all => qw(xconf xget_conf xget_pet xget_session xget_mri xput_rvr xput_report), usual => qw(xconf xget_conf xget_session));
 
 our $VERSION = 0.1;
@@ -66,29 +66,105 @@ sub xget_conf {
 	return %xconf;
 }
 
-=item xget_pet
+=item xget_session 
 
-Get the XNAT PET experiment ID
+Create a new JSESSIONID on XNAT. Return the connection data
+for the server AND the ID of the created session
 
 usage: 
 
-	xget_pet(host, jsession, project, subject)
+	xget_session();
 
 =cut
 
-sub xget_pet {
-	# Get the XNAT PET experiment ID
-	# usage: xget_pet(host, jsession, project, subject)
+sub xget_session {
+	# Create a new JSESSIONID on XNAT
+	# usage: xget_session(\%xconf);
+	#my %xdata = %{shift()};
+	my %xdata = xget_conf();
+	my $crd = 'curl -f -u '.$xdata{'USER'}.':'.$xdata{'PASSWORD'}.' -X POST '.$xdata{'HOST'}.'/data/JSESSION 2>/dev/null';
+	$xdata{'JSESSION'} = qx/$crd/;
+	return %xdata;
+}
+
+=item xget_subjects
+
+Get the list of subjects of a project into a HASH. 
+El HASH de input, I<%sbjs>, se construye como I<{ XNAT_ID =E<gt> Label }>
+
+usage: 
+
+	%sbjs = xget_subjects(host, jsession, project);
+
+=cut
+
+sub xget_subjects {
+	# Get the list of subjects of a project into a HASH
+	# usage: %sbjs = xget_subjects(host, jsession, project); 
+	# %sbjs se construye como { XNAT_ID => Label }
+	my %sbjs;
 	my @xdata = @_;
-	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].'/data/projects/'.$xdata[2].'/subjects/'.$xdata[3].'/experiments?format=json&xsiType=xnat:petSessionData" 2>/dev/null';
-	#print "$crd\n";
-	my $json_res = qx/$crd/;
-	my $exp_prop = decode_json $json_res;
-	my $xlab;
-	foreach my $experiment (@{$exp_prop->{'ResultSet'}{'Result'}}){
-		$xlab = $experiment->{'ID'};
+	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].'/data/projects/'.$xdata[2].'/subjects?format=csv&columns=ID,label" 2>/dev/null';
+	my @sbj_res = split '\n', qx/$crd/;
+	foreach my $sbj_prop (@sbj_res){
+		if ($sbj_prop =~ /^XNAT/){
+			my ($sid,$slabel) = $sbj_prop =~ /^(XNAT.+),(\S+),(.*)$/;
+			$sbjs{$sid}{'label'} = $slabel;
+		}
 	}
-	return $xlab;
+	return %sbjs;
+}
+
+=item xget_sbj_data
+
+Get the subjects metadata. Not too
+much interesting but to extract
+the subject label.
+
+usage:
+
+	$xdata = xget_sbj_data(host, jsession, subject, field);
+
+=cut
+
+sub xget_sbj_data {
+	# usage $xdata = xget_sbj_data(host, jsession, subject, field);
+	my @xdata = @_;
+	my $crd = 'curl -f -X GET -b "JSESSIONID='.$xdata[1].'" "'.$xdata[0].'/data/subjects/'.$xdata[2].'?format=json" 2>/dev/null';
+	my $jres = qx/$crd/;
+	my $xfres = decode_json $jres;
+	return $xfres->{items}[0]{data_fields}{$xdata[3]};
+}
+
+=item xget_exp_data
+
+Get a data field of an experiment.
+The desired field shoud be indicated as input.
+By example, if you want the date of the experiment this is 
+seeked as 
+
+	my $xdate = xget_exp_data($host, $session_id, $experiment, 'date')
+
+There are some common fields as I<date>, I<label> or I<dcmPatientId> 
+but in general  you should look at,
+
+	curl -X GET -b JSESSIONID=00000blahblah "http://myhost/data/experiments/myexperiment?format=json" 2>/dev/null | jq '.items[0].data_fields'
+
+in order to know the available fields
+
+usage:
+
+	$xdata = xget_exp_data(host, jsession, experiment, field);
+
+=cut
+
+sub xget_exp_data {
+	# usage $xdata = xget_exp_data(host, jsession, experiment, field);	
+	my @xdata = @_;
+	my $crd = 'curl -f -X GET -b "JSESSIONID='.$xdata[1].'" "'.$xdata[0].'/data/experiments/'.$xdata[2].'?format=json" 2>/dev/null';
+	my $jres = qx/$crd/;
+	my $xfres = decode_json $jres;
+	return $xfres->{items}[0]{data_fields}{$xdata[3]};
 }
 
 =item xget_mri
@@ -116,11 +192,13 @@ sub xget_mri {
 }
 
 =item xget_fs_data
+
 Get the full Freesurfer directory in a tar.gz file
 
 usage: 
 
 	xget_fs_data(host, jsession, project, experiment, output_path)
+	
 =cut
 
 sub xget_fs_data {
@@ -140,6 +218,59 @@ sub xget_fs_data {
 		return 1;
 	}else{
 		return 0;
+	}
+}
+
+=item xget_fs_stats
+
+Get a single stats file from Freesurfer segmentation
+
+usage:
+
+	xget_fs_stats(host, jsession, experiment, stats_file, output_file) 
+
+=cut
+
+sub xget_fs_stats {
+	my @xdata = @_;
+	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].'/data/experiments/'.$xdata[2].'/resources/FS/files?format=json" 2>/dev/null';
+	my $json_res = qx/$crd/;
+	my $fs_data = decode_json $json_res;
+	my $file_uri;
+	foreach my $var_data (@{$fs_data->{'ResultSet'}{'Result'}}){
+		if ((${$var_data}{'file_content'} eq 'FSstats') and (${$var_data}{'Name'} eq $xdata[3])){
+			$file_uri = ${$var_data}{'URI'};
+		}
+	}
+	if($file_uri){
+                $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].$file_uri.'" -o '.$xdata[4].' 2>/dev/null';
+		system($crd);
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+=item xget_fs_allstats
+
+Get all stats files from Freesurfer segmentation and write it down at selected directory
+
+usage:
+
+        xget_fs_allstats(host, jsession, experiment, output_dir)
+
+=cut
+
+sub xget_fs_allstats {
+	my @xdata = @_;
+	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].'/data/experiments/'.$xdata[2].'/resources/FS/files?format=json" 2>/dev/null';
+	my $json_res = qx/$crd/;
+	my $fs_data = decode_json $json_res;
+	foreach my $var_data (@{$fs_data->{'ResultSet'}{'Result'}}){
+		if ((${$var_data}{'file_content'} eq 'FSstats') and (${$var_data}{'Name'} =~ /.*\.stats$/)){
+			$crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].${$var_data}{'URI'}.'" -o '.$xdata[3].'/'.${$var_data}{'Name'}.' 2>/dev/null';
+			system($crd);
+		}
 	}
 }
 
@@ -169,173 +300,29 @@ sub xget_fs_qc {
 	return %qc;
 }
 
-=item xget_fs_stats
+=item xget_pet
 
-Get a single stats file from Freesurfer segmentation
+Get the XNAT PET experiment ID
 
-usage:
+usage: 
 
-	xget_fs_stats(host, jsession, experiment stats_file, output_file) 
+	xget_pet(host, jsession, project, subject)
+
 =cut
 
-sub xget_fs_stats {
+sub xget_pet {
+	# Get the XNAT PET experiment ID
+	# usage: xget_pet(host, jsession, project, subject)
 	my @xdata = @_;
-	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].'/data/experiments/'.$xdata[2].'/resources/FS/files?format=json" 2>/dev/null';
+	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].'/data/projects/'.$xdata[2].'/subjects/'.$xdata[3].'/experiments?format=json&xsiType=xnat:petSessionData" 2>/dev/null';
+	#print "$crd\n";
 	my $json_res = qx/$crd/;
-	my $fs_data = decode_json $json_res;
-	my $file_uri;
-	foreach my $var_data (@{$fs_data->{'ResultSet'}{'Result'}}){
-		if ((${$var_data}{'file_content'} eq 'FSstats') and (${$var_data}{'Name'} eq $xdata[3])){
-			$file_uri = ${$var_data}{'URI'};
-		}
+	my $exp_prop = decode_json $json_res;
+	my $xlab;
+	foreach my $experiment (@{$exp_prop->{'ResultSet'}{'Result'}}){
+		$xlab = $experiment->{'ID'};
 	}
-	if($file_uri){
-                $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].$file_uri.'" -o '.$xdata[4].' 2>/dev/null';
-		system($crd);
-		return 1;
-	}else{
-		return 0;
-	}
-}
-
-=item xget_session 
-
-Create a new JSESSIONID on XNAT. Return the connection data
-for the server AND the ID of the created session
-
-usage: 
-
-	xget_session();
-
-=cut
-
-sub xget_session {
-	# Create a new JSESSIONID on XNAT
-	# usage: xget_session(\%xconf);
-	#my %xdata = %{shift()};
-	my %xdata = xget_conf();
-	my $crd = 'curl -f -u '.$xdata{'USER'}.':'.$xdata{'PASSWORD'}.' -X POST '.$xdata{'HOST'}.'/data/JSESSION 2>/dev/null';
-	$xdata{'JSESSION'} = qx/$crd/;
-	return %xdata;
-}
-
-=item xput_report
-
-Upload a pdf report to XNAT
-
-usage: 
-
-	xput_report(host, jsession, subject, experiment, pdf_file);
-
-=cut
-
-sub xput_report{
-	# upload a pdf report to XNAT
-	# usage: xput_report(host, jsession, subject, experiment, pdf_file);
-	my @xdata = @_;
-	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X PUT "'.$xdata[0].'/data/experiments/'.$xdata[3].'/resources/RVR" 2>/dev/null';
-	system($crd);
-	$crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X PUT "'.$xdata[0].'/data/experiments/'.$xdata[3].'/resources/RVR/files/report_'.$xdata[2].'.pdf?overwrite=true" -F file="@'.$xdata[4].'"';
-       system($crd);       
-}
-
-=item xput_rvr
-
-Upload a JSON file with VR data
-
-usage: 
-
-	xput_rvr(host, jsession, experiment, json_file);
-
-=cut
-
-sub xput_rvr {
-	# Upload a JSON file with VR data
-	# usage: xput_rvr(host, jsession, experiment, json_file);
-	my @xdata = @_;
-	my $crd = 'curl -f -X PUT -b JSESSIONID='.$xdata[1].' "'.$xdata[0].'/data/experiments/'.$xdata[2].'/resources/RVR/files/report_data.json?overwrite=true" -F file="@'.$xdata[3].'"';
-	system($crd);
-}
-
-=item xget_rvr
-
-Get VR results into a HASH. Output is a hash with filenames and URI of each element stored at RVR
-
-usage: 
-
-	xget_rvr(host, jsession, project, experiment);
-
-=cut
-
-sub xget_rvr {
-	# Get VR results into a HASH
-	# usage: xget_rvr(host, jsession, project, experiment);
-	# output is a hash with filenames and URI of each element stored at RVR
-	my @xdata = @_;
-	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].'/data/projects/'.$xdata[2].'/experiments/'.$xdata[3].'/resources/RVR/files?format=json" 2>/dev/null';
-	my $json_res = qx/$crd/;
-	my $rvr_prop = decode_json $json_res;
-	my %report_data;
-	foreach my $rvr_res (@{$rvr_prop->{'ResultSet'}{'Result'}}){
-		if ($rvr_res->{'Name'}){
-			$report_data{$rvr_res->{'Name'}} = $rvr_res->{'URI'};
-		}
-	}
-	return %report_data;
-}
-
-=item xget_rvr_data
-
-Get RVR JSON data into a hash
-
-usage: 
-
-	xget_rvr_data(host, jsession, URI);
-
-=cut
-
-sub xget_rvr_data {
-	# Get RVR JSON data into a hash
-	# usage: xget_rvr_data(host, jsession, URI);  
-	my @xdata = @_;
-	my %rvr_data;
-	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].$xdata[2].'" 2>/dev/null';
-	my $json_res = qx/$crd/;
-	my $report_data = decode_json $json_res;
-	foreach my $var_data (@{$report_data->{'ResultSet'}{'Result'}}){
-		foreach my $kdata (sort keys %{$var_data}){
-			$rvr_data{$kdata} = ${$var_data}{$kdata};
-		}
-	}
-	return %rvr_data;
-}
-
-=item xget_subjects
-
-Get the list of subjects of a project into a HASH. 
-El HASH de input, I<%sbjs>, se construye como I<{ XNAT_ID =E<gt> Label }>
-
-usage: 
-
-	%sbjs = xget_subjects(host, jsession, project);
-
-=cut
-
-sub xget_subjects {
-	# Get the list of subjects of a project into a HASH
-	# usage: %sbjs = xget_subjects(host, jsession, project); 
-	# %sbjs se construye como { XNAT_ID => Label }
-	my %sbjs;
-	my @xdata = @_;
-	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].'/data/projects/'.$xdata[2].'/subjects?format=csv&columns=ID,label" 2>/dev/null';
-	my @sbj_res = split '\n', qx/$crd/;
-	foreach my $sbj_prop (@sbj_res){
-		if ($sbj_prop =~ /^XNAT/){
-			my ($sid,$slabel) = $sbj_prop =~ /^(XNAT.+),(\S+),(.*)$/;
-			$sbjs{$sid}{'label'} = $slabel;
-		}
-	}
-	return %sbjs;
+	return $xlab;
 }
 
 =item xget_pet_reg
@@ -398,56 +385,127 @@ sub xget_pet_data {
 	return %xresult;
 }
 
-=item xget_exp_data
+=item xput_report
 
-Get a data field of an experiment.
-The desired field shoud be indicated as input.
-By example, if you want the date of the experiment this is 
-seeked as 
+Upload a pdf report to XNAT
 
-	my $xdate = xget_exp_data($host, $session_id, $experiment, 'date')
+usage: 
 
-There are some common fields as I<date>, I<label> or I<dcmPatientId> 
-but in general  you should look at,
-
-	curl -X GET -b JSESSIONID=00000blahblah "http://myhost/data/experiments/myexperiment?format=json" 2>/dev/null | jq '.items[0].data_fields'
-
-in order to know the available fields
-
-usage:
-
-	$xdata = xget_exp_data(host, jsession, experiment, field);
+	xput_report(host, jsession, subject, experiment, pdf_file);
 
 =cut
 
-sub xget_exp_data {
-	# usage $xdata = xget_exp_data(host, jsession, experiment, field);	
+sub xput_report{
+	# upload a pdf report to XNAT
+	# usage: xput_report(host, jsession, subject, experiment, pdf_file);
 	my @xdata = @_;
-	my $crd = 'curl -f -X GET -b "JSESSIONID='.$xdata[1].'" "'.$xdata[0].'/data/experiments/'.$xdata[2].'?format=json" 2>/dev/null';
-	my $jres = qx/$crd/;
-	my $xfres = decode_json $jres;
-	return $xfres->{items}[0]{data_fields}{$xdata[3]};
+	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X PUT "'.$xdata[0].'/data/experiments/'.$xdata[3].'/resources/RVR" 2>/dev/null';
+	system($crd);
+	$crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X PUT "'.$xdata[0].'/data/experiments/'.$xdata[3].'/resources/RVR/files/report_'.$xdata[2].'.pdf?overwrite=true" -F file="@'.$xdata[4].'"';
+       system($crd);       
 }
 
-=item xget_sbj_data
+=item xput_rvr
 
-Get the subjects metadata. Not too
-much interesting but to extract
-the subject label.
+Upload a JSON file with VR data
 
-usage:
+usage: 
 
-	$xdata = xget_sbj_data(host, jsession, subject, field);
+	xput_rvr(host, jsession, experiment, json_file);
 
 =cut
 
-sub xget_sbj_data {
-	# usage $xdata = xget_sbj_data(host, jsession, subject, field);
+sub xput_rvr {
+	# Upload a JSON file with VR data
+	# usage: xput_rvr(host, jsession, experiment, json_file);
 	my @xdata = @_;
-	my $crd = 'curl -f -X GET -b "JSESSIONID='.$xdata[1].'" "'.$xdata[0].'/data/subjects/'.$xdata[2].'?format=json" 2>/dev/null';
-	my $jres = qx/$crd/;
-	my $xfres = decode_json $jres;
-	return $xfres->{items}[0]{data_fields}{$xdata[3]};
+	my $crd = 'curl -f -X PUT -b JSESSIONID='.$xdata[1].' "'.$xdata[0].'/data/experiments/'.$xdata[2].'/resources/RVR/files/report_data.json?overwrite=true" -F file="@'.$xdata[3].'"';
+	system($crd);
+}
+
+=item xcreate_res 
+
+Create an empty experiment resource
+
+usage:
+
+	xcreate_res(host, jsession, experiment, name)
+
+=cut
+
+sub xcreate_res {
+	my @xdata = @_;
+	my $crd = 'curl -f -X PUT -b JSESSIONID='.$xdata[1].' "'.$xdata[0].'/data/experiments/'.$xdata[2].'/resources/'.$xdata[3].'" 2>/dev/null';
+	system($crd);
+}
+
+=item xput_res 
+
+Upload data to an experiment resource
+
+usage:
+
+	xput_res(host, jsession, experiment, type, file, filename)
+
+=cut
+
+sub xput_res {
+	my @xdata = @_;
+	my $crd = 'curl -f -X PUT -b JSESSIONID='.$xdata[1].' "'.$xdata[0].'/data/experiments/'.$xdata[2].'/resources/'.$xdata[3].'/files/'.$xdata[4].'?overwrite=true" -F file="@'.$xdata[5].'"';
+	system($crd);
+}
+
+=item xget_rvr
+
+Get VR results into a HASH. Output is a hash with filenames and URI of each element stored at RVR
+
+usage: 
+
+	xget_rvr(host, jsession, project, experiment);
+
+=cut
+
+sub xget_rvr {
+	# Get the list of VR results into a HASH
+	# usage: xget_rvr(host, jsession, project, experiment);
+	# output is a hash with filenames and URI of each element stored at RVR
+	my @xdata = @_;
+	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].'/data/projects/'.$xdata[2].'/experiments/'.$xdata[3].'/resources/RVR/files?format=json" 2>/dev/null';
+	my $json_res = qx/$crd/;
+	my $rvr_prop = decode_json $json_res;
+	my %report_data;
+	foreach my $rvr_res (@{$rvr_prop->{'ResultSet'}{'Result'}}){
+		if ($rvr_res->{'Name'}){
+			$report_data{$rvr_res->{'Name'}} = $rvr_res->{'URI'};
+		}
+	}
+	return %report_data;
+}
+
+=item xget_rvr_data
+
+Get RVR JSON data into a hash
+
+usage: 
+
+	xget_rvr_data(host, jsession, URI);
+
+=cut
+
+sub xget_rvr_data {
+	# Get single RVR JSON data into a hash
+	# usage: xget_rvr_data(host, jsession, URI);  
+	my @xdata = @_;
+	my %rvr_data;
+	my $crd = 'curl -f -b JSESSIONID='.$xdata[1].' -X GET "'.$xdata[0].$xdata[2].'" 2>/dev/null';
+	my $json_res = qx/$crd/;
+	my $report_data = decode_json $json_res;
+	foreach my $var_data (@{$report_data->{'ResultSet'}{'Result'}}){
+		foreach my $kdata (sort keys %{$var_data}){
+			$rvr_data{$kdata} = ${$var_data}{$kdata};
+		}
+	}
+	return %rvr_data;
 }
 
 =back
