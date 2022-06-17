11ba4bf328b5e38525f9cbd4b62b868552f97e27
asqwerty666 <asqwerty@gmail.com>
2022-06-17 09:31:24 +0200

    testing XNAT API with Perl functions

--- tools/xtester.pl
+++ tools/xtester.pl
@@ -4,15 +4,13 @@
 #
 use strict;
 use warnings;
-use XNATACE qw(xget_conf xget_session xget_sbj_data);
+use XNATACE qw(xget_conf xget_session xget_fs_stats);
 use Data::Dump qw(dump);
 
-my %conn = xget_conf();
-
 my $xprj = 'f5cehbi';
-my $xpx = 'XNAT5_E00704';
+my $xpx = 'XNAT_E00105';
 my $sbj = 'XNAT_S00086';
 
-my $jid = xget_session(\%conn);
-my $xdata = xget_sbj_data($conn{'HOST'}, $jid, $sbj, 'label');
+my %conn = xget_session();
+my $xdata = xget_fs_stats($conn{'HOST'}, $conn{'JSESSION'}, $xpx, 'aseg.stats', '/nas/osotolongo/tmp/kakakaka.txt');
 print "$xdata\n";
