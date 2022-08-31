755d9bb998bc04b50a260318069d87aac8c3b4ac
asqwerty666 <asqwerty@gmail.com>
2022-08-31 12:35:45 +0200

    cat get commam separated list of parameters from xnat

--- bin/xnat_get_param.pl
+++ bin/xnat_get_param.pl
@@ -15,32 +15,59 @@ use Math::Round;
 
 my $xprj;
 my $oxfile;
+my $xvar;
 while (@ARGV and $ARGV[0] =~ /^-/) {
 	$_ = shift;
 	last if /^--$/;
 	if (/^-o/) {$oxfile = shift; chomp($oxfile);}
 	if (/^-x/) {$xprj = shift; chomp($xprj);}
+	if (/^-v/) {$xvar = shift; chomp($xvar);}
 }
 die "Should supply XNAT project" unless $xprj;
-$oxfile = $xprj.'_age_data.csv' unless $oxfile;
+$xvar =~ s/,/_/g;
+my @avar = split '_',$xvar;
+$oxfile = $xprj.'_'.$xvar.'_data.csv' unless $oxfile;
 my %xconf = xget_session();
 my $jid = $xconf{'JSESSION'};
 my %subjects = xget_subjects($xconf{'HOST'}, $xconf{'JSESSION'}, $xprj);
 foreach my $sbj (sort keys %subjects){
 	$subjects{$sbj}{'experiment'} = xget_mri($xconf{'HOST'}, $xconf{'JSESSION'}, $xprj, $sbj);
-	my $mri_date = xget_exp_data($xconf{'HOST'}, $xconf{'JSESSION'}, $subjects{$sbj}{'experiment'}, 'date');
-	my $dob = xget_sbj_demog($xconf{'HOST'}, $xconf{'JSESSION'}, $sbj, 'dob');
-		if ($mri_date and $dob){
-			my $ddif = Delta_Format(DateCalc(ParseDate($dob),ParseDate($mri_date)),2,"%hh")/(24*365.2425);
-			$subjects{$sbj}{'age'} = nearest(0.1, $ddif);
-			print "$subjects{$sbj}{'label'}, $subjects{$sbj}{'age'}\n";
+	print "$subjects{$sbj}{'label'}";
+	foreach my $axvar (@avar){
+		if ($axvar eq 'age'){
+			my $mri_date = xget_exp_data($xconf{'HOST'}, $xconf{'JSESSION'}, $subjects{$sbj}{'experiment'}, 'date');
+			my $dob = xget_sbj_demog($xconf{'HOST'}, $xconf{'JSESSION'}, $sbj, 'dob');
+			if ($mri_date and $dob){
+				my $ddif = Delta_Format(DateCalc(ParseDate($dob),ParseDate($mri_date)),2,"%hh")/(24*365.2425);
+				$subjects{$sbj}{'age'} = nearest(0.1, $ddif);
+			}
+		}else{
+			my $foo_var = xget_sbj_demog($xconf{'HOST'}, $xconf{'JSESSION'}, $sbj, $axvar);
+			if ($foo_var){
+				$subjects{$sbj}{$axvar} = $foo_var;
+			}
 		}
-}
-open ODF, ">$oxfile";
-print ODF "Subject_ID,AGE\n";
-foreach my $subject (sort keys %subjects){
-	if(exists($subjects{$subject}{'age'})){
-		print ODF "$subjects{$subject}{'label'},$subjects{$subject}{'age'}\n";
+		if(exists($subjects{$sbj}{$axvar}) and $subjects{$sbj}{$axvar}){
+			print ", $subjects{$sbj}{$axvar}";
+		}
 	}
+	print "\n";
+}
+(my $nvar = $xvar) =~ s/(\w+)/\u$1/;
+open ODF, ">$oxfile";
+print ODF "Subject_ID";
+foreach my $axvar (@avar){
+	(my $nvar = $axvar) =~ s/(\w+)/\u$1/;
+	print ODF ",$nvar";
+}
+print ODF "\n";
+foreach my $subject (sort keys %subjects){
+	print ODF "$subjects{$subject}{'label'}";
+	foreach my $axvar (@avar){
+		if(exists($subjects{$subject}{$axvar})){
+			print ODF ",$subjects{$subject}{$axvar}";
+		}
+	}
+	print ODF "\n";
 }
 close ODF;
