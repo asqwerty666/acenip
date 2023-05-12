c56f60bdbee5afe9a97b454ac1263517d6f5a771
asqwerty666 <asqwerty@gmail.com>
2023-05-12 10:55:09 +0200

    to remove windows shit inside cells m)

--- tools/xls2csv.pl
+++ tools/xls2csv.pl
@@ -8,13 +8,19 @@ my $excel = Spreadsheet::XLSX -> new ($ifile);
 foreach my $sheet (@{$excel -> {Worksheet}}) {
 	$sheet -> {MaxRow} ||= $sheet -> {MinRow};
 	foreach my $row ($sheet -> {MinRow} .. $sheet -> {MaxRow}) {
+		my $wrow = '';
 		$sheet -> {MaxCol} ||= $sheet -> {MinCol};
 		foreach my $col ($sheet -> {MinCol} ..  $sheet -> {MaxCol}) {
+			my $val = '';
 			my $cell = $sheet -> {Cells} [$row] [$col];
-			print($cell -> {Val});
-			print ",";
+			$val =  $cell -> {Val} if defined($cell -> {Val});
+			$val =~ s/\n/ /g;
+			$val =~ s/,/ /g;
+			$val =~ tr/\015//d;
+			$wrow .= $val.',';
 		}
-		print "\n";
+		$wrow =~ s/,$//;
+		print "$wrow\n";
 	 }
 	 print "\n";
 }
